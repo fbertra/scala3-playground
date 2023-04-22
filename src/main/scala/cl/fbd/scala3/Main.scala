e1ae1173cf47485625436ba87fd007c31724dd1d
fbertra <fbertra@gmail.com>
2023-04-22 00:25:31 +0000

    boundary, break (scala 3.3)

--- src/main/scala/cl/fbd/scala3/Main.scala
+++ src/main/scala/cl/fbd/scala3/Main.scala
@@ -1,11 +1,19 @@
 package cl.fbd.scala3
 
+import util.boundary, boundary.break
+
 @main
 def main () = 
     println ("hola pruebas scala 3")
 
-    val list = "abc" :: "123" :: Nil
+    val list = "abc" :: "123" :: "xyz" :: "a8b1" :: Nil
 
-    for sz <- list do
-        println (sz)
-    end
+    val primeraCadenaConDigito = boundary:
+        for sz <- list do
+            println (sz)
+
+            if sz.exists (Character.isDigit) then break (sz)
+
+        "ninguna cadena contiene digitos"
+
+    println (s"La primera cadena que contiene digitos es $primeraCadenaConDigito")
