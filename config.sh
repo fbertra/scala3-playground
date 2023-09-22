1ab9c6adcf6cbe823babf0d5104832d06f173cd0
fbertra <fbertra@gmail.com>
2023-09-22 22:55:03 +0000

    mejora instalacion cs

--- config.sh
+++ config.sh
@@ -1,6 +1,11 @@
 cd $HOME
 
+mkdir bin
+cd bin
+
 # curl -fL https://github.com/coursier/coursier/releases/latest/download/cs-x86_64-pc-linux.gz | gzip -d > cs && chmod +x cs && ./cs setup
+
+curl -fL https://github.com/coursier/coursier/releases/latest/download/cs-x86_64-pc-linux.gz | gzip -d > cs && chmod +x cs
 
 # sudo apt install clang lldb lld
 
