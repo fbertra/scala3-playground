3c7c3575c444d538fd10e46526a8723503de056f
fbertra <fbertra@gmail.com>
2023-09-24 00:37:44 +0000

    gitpod elimina insalaciones

--- config.sh
+++ config.sh
@@ -3,12 +3,10 @@ cd $HOME
 mkdir bin
 cd bin
 
-# curl -fL https://github.com/coursier/coursier/releases/latest/download/cs-x86_64-pc-linux.gz | gzip -d > cs && chmod +x cs && ./cs setup
+curl -fL https://github.com/coursier/coursier/releases/latest/download/cs-x86_64-pc-linux.gz | gzip -d > cs && chmod +x cs && ./cs setup
 
-curl -fL https://github.com/coursier/coursier/releases/latest/download/cs-x86_64-pc-linux.gz | gzip -d > cs && chmod +x cs
-
-# sudo apt install clang lldb lld
+sudo apt install clang lldb lld
 
 sudo apt install libstdc++-12-dev
 
-# sudo apt install libgc-dev
+sudo apt install libgc-dev
