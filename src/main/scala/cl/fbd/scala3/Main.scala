095af5b7fb1d207a8640866154c32023f4a9b442
fbertra <fbertra@gmail.com>
2023-10-14 12:34:18 +0000

    Option [String] -> String | Null

--- src/main/scala/cl/fbd/scala3/Main.scala
+++ src/main/scala/cl/fbd/scala3/Main.scala
@@ -7,6 +7,7 @@ import sttp.client4.Response
 
 import cl.fbd.scala3.domain.PokemonAblility
 import cl.fbd.scala3.domain.PokemonSvc
+import cl.fbd.scala3.domain.PokemonData
 
 
 @main
@@ -21,12 +22,31 @@ def main () =
 
     val sz = write (pa)
 
+    // testParse ()
+
     val pokemons = callPokemonAbility (5)
+    println (pokemons)
 
-    pokemons.foreach (println (_))
+def testParse () =
+    val test = """{"name":"bulbasaur","url":"https://pokeapi.co/api/v2/pokemon/1/"}"""
+
+    println ("one")
+    val res = read [PokemonData] (test)    
+    println (res)
+
+    println ("seq")
+    val test_seq = """[{"name":"bulbasaur","url":"https://pokeapi.co/api/v2/pokemon/1/"},{"name":"ivysaur","url":"https://pokeapi.co/api/v2/pokemon/2/"}]"""
+    val res_seq = read [Seq [PokemonData]] (test_seq)    
+    println (res_seq.mkString ("\n"))
+
+    println ("srv")
+                    //01234567890123456789
+    val test_srv = """{"count":1292,"next":"https://pokeapi.co/api/v2/pokemon?offset=5&limit=5","previous":null,"results":[{"name":"bulbasaur","url":"https://pokeapi.co/api/v2/pokemon/1/"},{"name":"ivysaur","url":"https://pokeapi.co/api/v2/pokemon/2/"},{"name":"venusaur","url":"https://pokeapi.co/api/v2/pokemon/3/"},{"name":"charmander","url":"https://pokeapi.co/api/v2/pokemon/4/"},{"name":"charmeleon","url":"https://pokeapi.co/api/v2/pokemon/5/"}]}"""
+    read [PokemonSvc] (test_srv) 
+    println (test_srv)
 
 
-def callPokemonAbility (limit: Int): Seq [PokemonSvc] =
+def callPokemonAbility (limit: Int): PokemonSvc =
     val pokemonApiURL = uri"https://pokeapi.co/api/v2/pokemon?limit=${limit}"
     val pokemonAbilityURL = "https://pokeapi.co/api/v2/ability/"
 
@@ -36,5 +56,5 @@ def callPokemonAbility (limit: Int): Seq [PokemonSvc] =
 
     println (response.body)
 
-    read [Seq [PokemonSvc]] (response.body)    
+    read [PokemonSvc] (response.body)    
     
