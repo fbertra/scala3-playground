7ff9167fcd31e49b328ff12a9e4557c1dc3ff5c4
fbertra <fbertra@gmail.com>
2024-01-20 19:52:06 +0000

    dead code, imports

--- src/main/scala/cl/fbd/scala3/Main.scala
+++ src/main/scala/cl/fbd/scala3/Main.scala
@@ -1,18 +1,13 @@
 package cl.fbd.scala3
 
-// import scala.concurrent.Future
-// import scala.concurrent.ExecutionContext
-// import scala.concurrent.Await
-// import concurrent.duration.DurationInt
-
-import cl.fbd.scala3.domain.PokemonAblility
-import cl.fbd.scala3.domain.PokemonSrv
-import cl.fbd.scala3.domain.PokemonData
-
 import gears.async.Future
 import gears.async.Future.awaitAll
 import gears.async.Async
 import gears.async.default.given
+
+import cl.fbd.scala3.domain.PokemonAblility
+import cl.fbd.scala3.domain.PokemonSrv
+import cl.fbd.scala3.domain.PokemonData
 
 import cl.fbd.scala3.client.callPokemonSrv
 import cl.fbd.scala3.client.callPokemonAbility
@@ -27,8 +22,6 @@ def main(limit: Int) =
 
 def getPokemonsAbility(limit: Int): Seq[PokemonAblility] =
   val pokemon_srv = callPokemonSrv(limit)
-
-  // given ExecutionContext = ExecutionContext.global
 
   Async.blocking:
     val futures = for pokemon <- pokemon_srv.results yield Future(callPokemonAbility(pokemon))
